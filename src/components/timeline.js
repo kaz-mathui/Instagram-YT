e10608a375bb789bb342ee2bafc80513e3fa890e
kaz-mathui <kazmat.hui@gmail.com>
2021-05-06 09:42:48 +0900

    packageを修正

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,3 +1,7 @@
-export default function Header() {
-  return <p>I am the header</p>;
+export default function Timeline() {
+  return (
+    <div className="container col-span-2">
+      <p>I am the timeline</p>
+    </div>
+  );
 }
